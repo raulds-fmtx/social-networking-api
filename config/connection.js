916ceae4a8c1972e94ac8a87a385aa1ee8774240
raulds-fmtx <rdsflomo03@gmail.com>
2024-07-08 19:32:49 -0500

    Seed data added

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,6 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social_network_db",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social_network_db");
 
 module.exports = mongoose.connection;
